c9ba74c2ff3c9836448719fe11e5c40a5021d954
Greg Allan <gregallandev@gmail.com>
2025-11-15 11:42:44 -0700

    Add keybindings for searching hidden and all files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -412,7 +412,7 @@ require('lazy').setup({
             local builtin = require 'telescope.builtin'
             vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
             vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [f]iles' })
             vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
             vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
             vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -438,6 +438,21 @@ require('lazy').setup({
                     prompt_title = 'Live Grep in Open Files',
                 }
             end, { desc = '[S]earch [/] in Open Files' })
+
+            vim.keymap.set('n', '<leader>sF', function()
+                builtin.find_files {
+                    hidden = true,
+                    prompt_title = 'Search files (w/ hidden)',
+                }
+            end, { desc = '[S]earch [Files] w/hidden' })
+
+            vim.keymap.set('n', '<leader>sa', function()
+                builtin.find_files {
+                    hidden = true,
+                    no_ignore = true,
+                    prompt_title = 'Search files (w/ hidden)',
+                }
+            end, { desc = '[S]earch [Files] w/hidden' })
 
             -- Shortcut for searching your Neovim configuration files
             vim.keymap.set('n', '<leader>sn', function()
